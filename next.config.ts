28f34069cd7b1bf73a0b2825a1ecdd5c48dadf7e
Eztas <nietzsche2669sensei@gmail.com>
2025-07-17 17:37:40 +0900

    update: 本番環境用と開発環境用でパスを変えられるようにする

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV == "production"
+
 const nextConfig: NextConfig = {
   // これが最も重要！静的サイトとして出力する設定
   output: 'export',
 
   // GitHub Pagesでリポジトリ名をサブディレクトリとして使う場合の設定
   // 例: https://<username>.github.io/<repository-name>
-  basePath: '/next-jump-sprint',
-  assetPrefix: '/next-jump-sprint/',
+  basePath: isProd ? '/next-jump-sprint' : '',
+  assetPrefix: isProd ? '/next-jump-sprint/' : '',
 
   // 画像最適化を無効にする（静的エクスポートでは必須）
   images: {
