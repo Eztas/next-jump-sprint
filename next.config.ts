e6dd55f6c0ae719f6e61404dd0e454ff3ffc9241
Eztas <nietzsche2669sensei@gmail.com>
2025-07-18 07:25:02 +0900

    fix: basePathを通す

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = 'next-jump-sprint'; // 自身のGitHubリポジトリ名に変更してください
+
 const nextConfig: NextConfig = {
   // これが最も重要！静的サイトとして出力する設定
   output: 'export',
+    // 本番環境のみリポジトリ名をパスのプレフィックスとして設定
+  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '',
 };
 
 // export default nextConfig;
