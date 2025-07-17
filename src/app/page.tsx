00288410a150912a852cc57eaeb2eacea44b3644
Eztas <nietzsche2669sensei@gmail.com>
2025-07-18 07:55:59 +0900

    fix: next/imageもimgにしてみる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          <img
             aria-hidden
             src={`${basePath}/file.svg`}
             alt="File icon"
