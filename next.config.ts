cb03d55296cd46e6a8d096aeeff645a7e1914dbc
Eztas <nietzsche2669sensei@gmail.com>
2025-07-17 22:05:51 +0900

    fix: pathに関してconfig.tsとymlで競合しているかもしれないのでconfig側を消す

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,6 @@ const nextConfig: NextConfig = {
   // これが最も重要！静的サイトとして出力する設定
   output: 'export',
 
-  // GitHub Pagesでリポジトリ名をサブディレクトリとして使う場合の設定
-  // 例: https://<username>.github.io/<repository-name>
-  basePath: isProd ? '/next-jump-sprint' : '',
-  assetPrefix: isProd ? '/next-jump-sprint/' : '',
-
   // 画像最適化を無効にする（静的エクスポートでは必須）
   images: {
     unoptimized: true,
