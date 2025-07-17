bc2ac04bc8f8cf6e6c4a2056437a50445109021d
Eztas <nietzsche2669sensei@gmail.com>
2025-07-18 07:13:20 +0900

    fix: ymlで画像最適化も無効化できそうなので、競合部分削除

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV == "production"
-
 const nextConfig: NextConfig = {
   // これが最も重要！静的サイトとして出力する設定
   output: 'export',
-
-  // 画像最適化を無効にする（静的エクスポートでは必須）
-  images: {
-    unoptimized: true,
-  },
 };
 
 // export default nextConfig;
