6e3eca643cad7c674d17a06a8ea48e12d0ab5241
Eztas <nietzsche2669sensei@gmail.com>
2025-07-18 07:37:56 +0900

    fix: やはりpage.tsxにパスを通すしかなさそうなので一度やってみる

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,17 @@ import type { NextConfig } from "next";
 
 const repoName = 'next-jump-sprint'; // 自身のGitHubリポジトリ名に変更してください
 
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV == "production"
+
 const nextConfig: NextConfig = {
   // これが最も重要！静的サイトとして出力する設定
   output: 'export',
-    // 本番環境のみリポジトリ名をパスのプレフィックスとして設定
-  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '',
-
-  // GitHub Pagesでは画像最適化が機能しないため無効にする
-  images: {
-    unoptimized: true,
+  // 本番環境のみリポジトリ名をパスのプレフィックスとして設定
+  basePath: isProd ? repoName : "",
+  assetPrefix: isProd ? repoName : "",
+  publicRuntimeConfig: {
+    basePath: isProd ? repoName : "",
   },
 };
 
