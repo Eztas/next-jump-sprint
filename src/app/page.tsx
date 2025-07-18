6100ecfd95bd5f3a5a3f2bd5dc714361f708e1d6
Eztas <nietzsche2669sensei@gmail.com>
2025-07-18 20:05:53 +0900

    wip: bug: revert: windows以外の画像パス全てがまた通らなくなったので一度戻す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
         >
           <img
             aria-hidden
-            src="/windows.svg"
+            src="/window.svg"
             alt="Window icon"
             width={16}
             height={16}
