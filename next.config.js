dbea3fff132cc86d5560a8875cb11ab46df23ff2
Eztas <nietzsche2669sensei@gmail.com>
2025-07-18 19:42:32 +0900

    fix: configをtsからjsに変えてみる

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-import type { NextConfig } from "next";
-
 const repoName = '/next-jump-sprint'; // 自身のGitHubリポジトリ名に変更してください
 
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV == "production"
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   // これが最も重要！静的サイトとして出力する設定
   output: 'export',
   // 本番環境のみリポジトリ名をパスのプレフィックスとして設定
